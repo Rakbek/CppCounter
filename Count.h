5fb2c16080a0bfb81e88b270d05d39bd7b46f18a
Michele delle Noci <michele.dellenoci@gmail.com>
2019-10-10 23:24:02 +0200

    Add Count and Integer_Count header

---
+++ Count.h
@@ -0,0 +1,26 @@
+template<class Count_Type>
+class Count {
+    public:
+
+        // Constructors/destructor
+        Count() {}
+        virtual ~Count() {}
+
+        // Implementors
+        virtual void increment() = 0;
+        virtual void decrement() = 0;
+        void reset() { value = reset_value; }
+
+        // Accessor
+        Count_Type get_value() { return value; }
+        void set_value(Count_Type new_value) { value = new_value; }
+
+        Count_Type get_reset_value() { return reset_value; }
+        void set_reset_value(Count_Type new_value) { reset_value = new_value; }
+
+    protected:
+        
+        // Data members
+        Count_Type value;
+        Count_Type reset_value;
+};
---
+++ Integer_Count.h
@@ -0,0 +1,16 @@
+#include <Count.h>
+
+class Integer_Count : public Count<int> {
+    
+    public:
+
+        // Constructors/destructors
+        Integer_Count() { reset_value = 0; reset(); }
+        Integer_Count(int new_value) { reset_value = 0; value = new_value; }
+        ~Integer_Count() { }
+
+        // Implementors
+        void increment();
+        void decrement();
+        char* asBase(int number_base);
+};
